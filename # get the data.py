71b6a8cec3801b5b7e60ac152f0bc8219fe29c13
Fmurgioni94 <fr.murgioni94@gmail.com>
2023-11-02 12:56:29 +0000

    up 4

--- # get the data.py
+++ # get the data.py
@@ -6,7 +6,7 @@ import hashlib
 from adjacency_matrix_graph import AdjacencyMatrixGraph
 from adjacency_list_graph import AdjacencyListGraph
 from dijkstra import dijkstra
-import networkx as nx
+# import networkx as nx
 import matplotlib.pyplot as plt
 
 
@@ -14,41 +14,36 @@ import matplotlib.pyplot as plt
 with open('LondonUnderground.csv', 'r') as f:
     # Create a reader object
     reader = csv.reader(f)
-    list_of_stops = []
-    list_of_edges = []
+    vertices = {}
+    edges = {}
+    i = 0
     for row in reader:
-        if row[1].strip() not in set(list_of_stops):
-            list_of_stops.append(row[1].strip())
+        if row[1].strip() not in vertices:
+            vertices[row[1].strip()] = i 
+            i += 1  
         if len(row) == 4 and row[2] != "":
-            if (row[2].strip(), row[1].strip(), row[3].strip()) not in set(list_of_edges) and (row[1].strip(), row[2].strip(), row[3].strip()) not in set(list_of_edges):
-                list_of_edges.append((row[1].strip(), row[2].strip(), row[3].strip()))
-    for i in range(len(list_of_edges)):
-        list_of_edges[i] = (list_of_edges[i][0],list_of_edges[i][1],int(list_of_edges[i][2]))
+            if (row[2].strip(), row[1].strip()) not in edges and (row[1].strip(), row[2].strip()) not in edges:
+                edges[(row[1].strip(), row[2].strip())] = int(row[3].strip())
 
-    graph1 = AdjacencyListGraph(len(list_of_stops), False, True)
+    graph1 = AdjacencyListGraph(len(vertices), False, True)
 
     #insert the starting and ending stations
     starting_station = input("Insert you starting point.")
     arriving_station = input("Insert you arriving station.")
 
     #we retrive the index now with the command index, but will be implemented with dictionary
-    starting_index = list_of_stops.index(starting_station)
-    arriving_index = list_of_stops.index(arriving_station)
+    starting_index = vertices[starting_station]
+    arriving_index = vertices[arriving_station]
 
 
     # thi part needs an update that consider the edges are now dictionary
     for edge in list_of_edges:
-        graph1.insert_edge(list_of_stops.index(edge[0]), list_of_stops.index(edge[1]), edge[2])
-    d, pi = dijkstra(graph1, list_of_stops.index(starting_station))
+        graph1.insert_edge(vertices[edge[0]], vertices[edge[1]], edges[edge])
+    d, pi = dijkstra(graph1, vertices[starting_station])
 
-    # for i in range(len(list_of_stops)):
-    #     print(list_of_stops[i] + ": d = " + str(d[i]) + ", pi = " + ("None" if pi[i] is None else list_of_stops[pi[i]]))
-    # print(pi)
-    # print(d)
-    #
-    # for i in range(len(list_of_stops)):
-    #     print(i, list_of_stops[i])
+    
     path = []
+    counter = 0
     while pi[arriving_index] is not None:
         time = d[arriving_index]
         next_station = pi[arriving_index]
